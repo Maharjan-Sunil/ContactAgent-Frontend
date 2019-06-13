b5d1b11e58ef3e00110f75fa234c52cf2f6b0c2f
sunil <sunil@codebee.dk>
2019-06-13 15:09:33 +0545

    ANG-3 code updated

--- src/app/services/register.service.ts
+++ src/app/services/register.service.ts
@@ -14,7 +14,7 @@ httpOptions = {
 };
 
 // baseUrl:string="http://api.ourfreeshare.com/api/token/"; //for live
-baseUrl:string="http://localhost:62215/api/token/"; //for local
+baseUrl:string="http://localhost:62215/api/token/"; //for  local
 
 register(credentials){
     return this.http.post<boolean>(this.baseUrl+"register",JSON.stringify(credentials),this.httpOptions);
